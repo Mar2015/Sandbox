c165da2090c8e23df5e269d3203a449eed0b3d23
Tomo Mizoe <mz@july.co.jp>
2015-04-09 20:42:45 +0900

    コメント追加、松谷追加

--- Console1/Console1/Program.cs
+++ Console1/Console1/Program.cs
@@ -12,6 +12,12 @@ namespace Console1
         {
             string[] names = new string[21];
             names[0] = "溝江 智徳";
+
+            // この下に追加してください。
+            // names[名簿番号の要素番号] = "名前";
+
+            names[21] = "松谷 春華";
+
             foreach (var name in names)
 	        {
 		        Console.WriteLine(name);
