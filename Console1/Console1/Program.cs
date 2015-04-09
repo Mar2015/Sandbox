239de9fe189b86e050efe7fc147b55bd0f9a4fbe
Tomo Mizoe <mz@july.co.jp>
2015-04-09 20:40:34 +0900

    first version

---
+++ Console1/Console1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = new string[21];
+            names[0] = "溝江 智徳";
+            foreach (var name in names)
+	        {
+		        Console.WriteLine(name);
+	        }
+            Console.ReadLine();
+        }
+    }
+}
